077b4a20431c5f5d7ffb8acca7dd9331b6d7603f
Anas-86 <anas.kala@hotmail.com>
2023-10-17 00:13:00 +0200

    Refactoring for the the first module.

--- Module1/VectorOperations.py
+++ Module1/VectorOperations.py
@@ -36,3 +36,19 @@ class Vector:
 
     def __mul__(self, scalar):
         return Vector(self.x * scalar, self.y * scalar)
+
+    # Include the following in main.py for operating on this class
+
+    # import Module1.VectorOperations as vec
+    #
+    # vec1 = vec.Vector(12, 9)
+    # vec2 = vec.Vector(3, 4)
+    #
+    # print(vec1)
+    # print(abs(vec1))
+    # if (vec1):
+    #    print("true")
+    # else:
+    #    print("false")
+    # print(vec1 + vec2)
+    # print(vec2 * 3)
